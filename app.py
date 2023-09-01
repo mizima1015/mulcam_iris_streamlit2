58013c9a5fa35d40082e62058ad48a86d1f29ebe
Mizima <mizimaaz@gmail.com>
2023-09-01 10:16:42 +0900

    menu

--- app.py
+++ app.py
@@ -11,7 +11,21 @@ import matplotlib.pyplot as plt
 def main():
 
     st.markdown("# Hello World")
-    st.write(np.__version__)
+    
+    menu = ["Home","탐색적 자료 분석", "머신러닝", "About"]
+    choice = st.sidebar.selectbox("메뉴",menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+    elif choice == "탐색적 자료 분석":
+        st.subheader("탐색석 자료 분석")
+    elif choice == "머신러닝":
+        st.subheader("머신러닝")
+    elif choice == "About":
+        st.subheader("About")
+    else:
+        pass
+    
 
 if __name__ == "__main__":
     main()
