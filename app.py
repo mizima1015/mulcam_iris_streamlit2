048a6cc932dfa94826a7547a3f6c03bd0995af37
Mizima <mizimaaz@gmail.com>
2023-09-01 09:59:25 +0900

    version

---
+++ app.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import plotly as plt
+import sklearn as skl
+import matplotlib.pyplot as plt
+
+def main():
+
+    st.markdown("# Hello World")
+    st.write(np.__version__)
+
+if __name__ == "__main__":
+    main()
